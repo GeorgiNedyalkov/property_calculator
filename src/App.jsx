3844ff978c1306fd508d4815075588128ffcd352
Flu3nt <gnedyalkov94@gmail.com>
2023-03-06 17:22:28 +0200

    Create Form Field Component. Add interest rate, property value and loan term form fieds

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,70 @@
+import { useState } from "react";
 import "./App.css";
 
+const INITIAL_STATE = {
+  propertyValue: 10000,
+  downpayment: 0.2,
+  term: 20,
+  interestRate: 3,
+};
+
 function App() {
+  const [form, setForm] = useState(INITIAL_STATE);
+
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: [e.target.value],
+    });
+  };
+
   return (
     <div className="App">
-      <h1>Property Calculator</h1>
+      <header>
+        <h1>Property Calculator</h1>
+      </header>
+
+      <main>
+        <form>
+          <FormField
+            label="Interest Rate"
+            name="interestRate"
+            value={form.interestRate}
+            handleChange={handleChange}
+          />
+
+          <FormField
+            label="Property Value"
+            name="propertyValue"
+            value={form.propertyValue}
+            handleChange={handleChange}
+          />
+
+          <FormField
+            label="Loan Term"
+            name="term"
+            value={form.term}
+            handleChange={handleChange}
+          />
+        </form>
+
+        <div className="display">
+          <div>Interest Rate: {form.interestRate}%</div>
+          <div>Property Value: ${form.propertyValue.toLocaleString()}</div>
+          <div>Loan Term: {form.term} years</div>
+        </div>
+      </main>
     </div>
   );
 }
 
 export default App;
+
+const FormField = ({ label, value, name, handleChange }) => {
+  return (
+    <div className="formField">
+      <label htmlFor={label}>{label} </label>
+      <input type="number" value={value} onChange={handleChange} name={name} />
+    </div>
+  );
+};
